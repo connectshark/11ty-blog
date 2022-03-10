88e1db2e6cc2e54517bac85257bda0bb9e868b61
Chambers <bobhus394@gmail.com>
2022-03-10 13:16:56 +0800

    字級設定

--- .eleventy.js
+++ .eleventy.js
@@ -1,28 +1,38 @@
 const markdownIt = require("markdown-it");
+const markdownItp = require('markdown-it')()
 const markdownItAnchor = require("markdown-it-anchor");
+const markdownItContainer = require('markdown-it-container')
 const pluginNavigation = require("@11ty/eleventy-navigation");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const slugify = require("slugify");
-const pluginTOC = require('eleventy-plugin-toc')
 const now = String(Date.now())
 module.exports = eleventyConfig => {
   eleventyConfig.addWatchTarget('src/css/**.css')
+
+
   eleventyConfig.addShortcode('version', function () {
     return now
   })
+
+  eleventyConfig.addFilter('postDate', dateObject => {
+    return dateObject
+  })
+
   eleventyConfig.addPassthroughCopy('src/assets')
   eleventyConfig.addPassthroughCopy('src/css/menu.css')
 
   eleventyConfig.addPlugin(pluginNavigation);
   eleventyConfig.addPlugin(pluginRss);
-  eleventyConfig.addPlugin(pluginSyntaxHighlight);
-  eleventyConfig.addPlugin(pluginTOC)
-  eleventyConfig.setLibrary("md", markdownIt({
+  eleventyConfig.addPlugin(pluginSyntaxHighlight)
+
+  let markdownLibrary = markdownIt({
     html: true,
     breaks: true,
     linkify: true
-  }).use(markdownItAnchor, {
+  })
+  .use(require("markdown-it-table-of-contents"))
+  .use(markdownItAnchor, {
     permalink: markdownItAnchor.permalink.ariaHidden({
       placement: "before",
       class: "direct-link",
@@ -36,7 +46,25 @@ module.exports = eleventyConfig => {
         })
     }),
     slugify: eleventyConfig.getFilter("slug")
-  }));
+  })
+  .use(markdownItContainer, 'summary', {
+    validate: function (params) {
+      return params.trim().match(/^spoiler\s+(.*)$/);
+    },
+    render: function (tokens, idx) {
+      var m = tokens[idx].info.trim().match(/^spoiler\s+(.*)$/);
+      if (tokens[idx].nesting === 1) {
+        return '<details><summary>' + markdownItp.utils.escapeHtml(m[1]) + '</summary>\n';
+      } else {
+        return '</details>\n'
+      }
+    }
+  })
+  .use(markdownItContainer, "success")
+  .use(markdownItContainer, "info")
+  .use(markdownItContainer, "warning")
+  .use(markdownItContainer, "danger")
+  eleventyConfig.setLibrary("md", markdownLibrary)
   return {
     dir: {
       input: 'src',
--- src/index.md
+++ src/index.md
@@ -3,4 +3,31 @@ title: 首頁
 layout: 'layouts/index.njk'
 ---
 
+[[toc]]
+
+
+- 第一個
+    - 第二個
 # welcome {{ title }}
+```
+hello
+```
+## 測試
+::: spoiler 小提醒
+測試一下
+:::
+
+## 第二段
+
+::: warning
+Custom Title Custom markdown texts
+:::
+::: success
+Custom Title Custom markdown texts
+:::
+::: info
+Custom Title Custom markdown texts
+:::
+::: danger
+Custom Title Custom markdown texts
+:::
